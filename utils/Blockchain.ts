9fb4b4bd143606a1916f61729a56445062f93ad9
tysonbattistella <tysonbattistella@gmail.com>
2020-10-21 20:10:18 -0700

    Initial Commit

---
+++ utils/Blockchain.ts
@@ -0,0 +1,37 @@
+import { JsonRpcProvider } from 'ethers/providers';
+
+export class Blockchain {
+  private _snapshotId: number;
+  private _provider: JsonRpcProvider;
+
+  constructor(provider: JsonRpcProvider) {
+    this._provider = provider;
+  }
+
+  public async saveSnapshotAsync(): Promise<void> {
+    const response = await this.sendJSONRpcRequestAsync('evm_snapshot', []);
+    this._snapshotId = Number(response);
+  }
+
+  public async revertAsync(): Promise<void> {
+    await this.sendJSONRpcRequestAsync('evm_revert', [this._snapshotId]);
+  }
+
+  public async resetAsync(): Promise<void> {
+    await this.sendJSONRpcRequestAsync('evm_revert', ['0x1']);
+  }
+
+  public async increaseTimeAsync(duration: number): Promise<any> {
+    await this.sendJSONRpcRequestAsync('evm_increaseTime', [duration]);
+  }
+
+  public async waitBlocksAsync(count: number) {
+    for (let i = 0; i < count; i++) {
+      await this.sendJSONRpcRequestAsync('evm_mine', []);
+    }
+  }
+
+  private async sendJSONRpcRequestAsync(method: string, params: any[]): Promise<any> {
+    return this._provider.send(method, params);
+  }
+}
