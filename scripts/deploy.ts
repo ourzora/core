d4ec998d8f3402382118735776b54a7791ca7cd3
tysonbattistella <tysonbattistella@gmail.com>
2020-11-07 19:05:07 -0800

    v0.1.1

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,7 +1,8 @@
 import fs from 'fs-extra';
 import { JsonRpcProvider } from '@ethersproject/providers';
-import { Wallet } from 'ethers';
-import { InvertFactory } from '../typechain/InvertFactory';
+import { Wallet } from '@ethersproject/wallet';
+import { MediaFactory } from '../typechain/MediaFactory';
+import { MarketFactory } from '../typechain/MarketFactory';
 
 async function start() {
   const args = require('minimist')(process.argv.slice(2));
@@ -14,23 +15,46 @@ async function start() {
   }`;
   await require('dotenv').config({ path });
   const provider = new JsonRpcProvider(process.env.RPC_ENDPOINT);
-  const wallet = new Wallet(process.env.PRIVATE_KEY, provider);
+  const wallet = new Wallet(`0x${process.env.PRIVATE_KEY}`, provider);
   const sharedAddressPath = `${process.cwd()}/addresses/${args.chainId}.json`;
   // @ts-ignore
   const addressBook = JSON.parse(await fs.readFileSync(sharedAddressPath));
-  if (addressBook.invert) {
+  if (addressBook.market) {
     throw new Error(
-      `invert already exists in address book at ${sharedAddressPath}. Please move it first so it is not overwritten`
+      `market already exists in address book at ${sharedAddressPath}. Please move it first so it is not overwritten`
+    );
+  }
+  if (addressBook.media) {
+    throw new Error(
+      `media already exists in address book at ${sharedAddressPath}. Please move it first so it is not overwritten`
     );
   }
 
-  console.log('Deploying Invert...');
-  const deployTx = await new InvertFactory(wallet).deploy();
+  console.log('Deploying Market...');
+  const deployTx = await new MarketFactory(wallet).deploy();
   console.log('Deploy TX: ', deployTx.deployTransaction.hash);
   await deployTx.deployed();
-  console.log('Invert deployed at ', deployTx.address);
-  addressBook.invert = deployTx.address;
+  console.log('Market deployed at ', deployTx.address);
+  addressBook.market = deployTx.address;
+
+  console.log('Deploying Media...');
+  const mediaDeployTx = await new MediaFactory(wallet).deploy(
+    addressBook.market
+  );
+  console.log(`Deploy TX: ${mediaDeployTx.deployTransaction.hash}`);
+  await mediaDeployTx.deployed();
+  console.log(`Media deployed at ${mediaDeployTx.address}`);
+  addressBook.media = mediaDeployTx.address;
+
+  console.log('Configuring Market...');
+  const market = MarketFactory.connect(addressBook.market, wallet);
+  const tx = await market.configure(addressBook.media);
+  console.log(`Market configuration tx: ${tx.hash}`);
+  await tx.wait();
+  console.log(`Market configured.`);
+
   await fs.writeFile(sharedAddressPath, JSON.stringify(addressBook, null, 2));
+  console.log(`Contracts deployed and configured. ☼☽`);
 }
 
 start().catch((e: Error) => {
