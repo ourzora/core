734bcd9f982c8c1ff9248fcf1ea9f4b786798542
fastackl <fastackl@protonmail.com>
2023-10-18 11:12:57 +1100

    deploying to goerli with fast utils

--- scripts/scripts-config.js
+++ scripts/scripts-config.js
@@ -2,115 +2,40 @@ module.exports = {
   localhost: {
     'deploy.js': [
       {
-        name: 'Decimal',
+        name: 'Market',
       },
-      // ,
-      // {
-      //     name: "CommonNFT"
-      // },
-      // {
-      //     name: "BasisPoints"
-      // },
-      // {
-      //     name: "DQ",
-      //     libraries: {"BasisPoints": "BasisPoints.address"}
-      // },
-      // {
-      //     name: "DQURI"
-      // },
-      // {
-      //     name: "Base64"
-      // },
-      // {
-      //     name: "RQ",
-      //     libraries: {"BasisPoints": "BasisPoints.address"},
-      //     constructorArgs: ["DQ.address"]
-      // },
-      // {
-      //     name: "DispatchLib"
-      // },
-      // {
-      //     name: "RQDQURI"
-      // },
-      // {
-      //     name: "RQDQ",
-      //     libraries: {"DispatchLib": "DispatchLib.address", "BasisPoints": "BasisPoints.address"},
-      //     constructorArgs: ["K21.address", "RQDQURI.address"]
-      // },
-      // {
-      //     name: "ReservationBook",
-      //     libraries: {"DispatchLib": "DispatchLib.address"},
-      //     constructorArgs: ["RQDQ.address"]
-      // }
+      {
+        name: 'Media',
+        constructorArgs: ['Market.address'],
+      },
     ],
     'initialize.js': [
-      // {
-      //     contract: "DQ",
-      //     function: "initialize",
-      //     args: ["RQ.address","DQURI.address"]
-      // },
-      // {
-      //     contract: "RQDQ",
-      //     function: "initialize",
-      //     args: ["ReservationBook.address"]
-      // }
+      {
+        contract: 'Market',
+        function: 'configure',
+        args: ['Media.address'],
+      },
     ],
-    // ,
     // "verify.js": ["+REST"]
   },
   goerli: {
     'deploy.js': [
       {
-        name: 'Decimal',
+        name: 'Market',
       },
-      // {
-      //     name: "BasisPoints"
-      // },
-      // {
-      //     name: "DQ",
-      //     libraries: {"BasisPoints": "BasisPoints.address"}
-      // },
-      // {
-      //     name: "DQURI"
-      // },
-      // {
-      //     name: "Base64"
-      // },
-      // {
-      //     name: "RQ",
-      //     libraries: {"BasisPoints": "BasisPoints.address"},
-      //     constructorArgs: ["DQ.address"]
-      // },
-      // {
-      //     name: "DispatchLib"
-      // },
-      // {
-      //     name: "RQDQURI"
-      // },
-      // {
-      //     name: "RQDQ",
-      //     libraries: {"DispatchLib": "DispatchLib.address", "BasisPoints": "BasisPoints.address"},
-      //     constructorArgs: ["K21.address", "RQDQURI.address"]
-      // },
-      // {
-      //     name: "ReservationBook",
-      //     libraries: {"DispatchLib": "DispatchLib.address"},
-      //     constructorArgs: ["RQDQ.address"]
-      // }
+      {
+        name: 'Media',
+        constructorArgs: ['Market.address'],
+      },
     ],
-    // "initialize.js": [
-    //     {
-    //         contract: "DQ",
-    //         function: "initialize",
-    //         args: ["RQ.address","DQURI.address"]
-    //     },
-    //     {
-    //         contract: "RQDQ",
-    //         function: "initialize",
-    //         args: ["ReservationBook.address"]
-    //     }
-    // ],
-    'verify.js': ['Decimal'],
+    'initialize.js': [
+      {
+        contract: 'Market',
+        function: 'configure',
+        args: ['Media.address'],
+      },
+    ],
+    'verify.js': ['+REST'],
   },
   // "mainnet": {
   //     "deploy.js": [
